256747f25ccfae7afb2da8fb4e8e48a137cb3e9a
Mohamed Sabbar <msabbar1@educacion.navarra.es>
2025-10-10 09:31:11 +0200

    añadir el player y su controller y un collected para comprobar si esta tocando el suelo , añadir la fruta como prefab , el player puede hacer un salto perfecto si mantengo la tecla espacio presionada ,anadir un material 2D al rigidbody del suelo.

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float runSpeed = 2;
+    public float jumpSpeed = 3;
+    Rigidbody2D rb2D;
+    
+    public bool  betterJump = false;
+    public float fallMultiplier = 0.5f;
+    public float lowJumpMultiplier = 1f;
+
+    public SpriteRenderer spriteRenderer;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2D.velocity = new Vector2(runSpeed, rb2D.velocity.y);
+            spriteRenderer.flipX = false;
+            animator.SetBool("Run", true);
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2D.velocity = new Vector2(-runSpeed, rb2D.velocity.y);
+            spriteRenderer.flipX = true;
+            animator.SetBool("Run", true);
+        }
+        else
+        {
+            rb2D.velocity = new Vector2(0, rb2D.velocity.y);
+            animator.SetBool("Run", false);      
+        }
+
+        if (Input.GetKey("space") && CheckGround.isGrounded)
+        {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+        }
+
+        if (betterJump)
+        {
+            if (rb2D.velocity.y < 0)
+            {
+                rb2D.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier) * Time.fixedDeltaTime;
+            }
+            else if (rb2D.velocity.y > 0 && !Input.GetKey("space"))
+            {
+                rb2D.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier) * Time.fixedDeltaTime;
+            }
+        }
+        
+    }
+}
